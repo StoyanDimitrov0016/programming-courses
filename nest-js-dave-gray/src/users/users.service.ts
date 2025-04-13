d7c672fdeff206d54d7ba7b0af75711397b3570d
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-04-13 20:57:44 +0300

    feat: create users service CRUD methods for the corresponding controller

--- nest-js-dave-gray/src/users/users.service.ts
+++ nest-js-dave-gray/src/users/users.service.ts
@@ -1,4 +1,58 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    { id: 1, name: 'John Doe', email: 'john.doe@mail.com', role: 'INTERN' },
+    { id: 2, name: 'Jane Smith', email: 'jane.s@mail.com', role: 'ENGINEER' },
+    { id: 3, name: 'Bob Johnson', email: 'bob.j@mail.com', role: 'ADMIN' },
+    { id: 4, name: 'Alice Brown', email: 'alice.b@mail.com', role: 'INTERN' },
+    { id: 5, name: 'Tom White', email: 'tom.w@mail.com', role: 'ENGINEER' },
+  ];
+
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  createOne(data: {
+    name: string;
+    email: string;
+    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+  }) {
+    const id = this.users.length++;
+    const user = { id, ...data };
+
+    this.users.push(user);
+    return user;
+  }
+
+  updateOne(
+    id: number,
+    data: Partial<{
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    }>,
+  ) {
+    const position = this.users.findIndex((user) => user.id === id);
+    const user = this.users[position];
+    const updated = { ...user, ...data };
+
+    this.users[position] = updated;
+    return updated;
+  }
+
+  deleteOne(id: number) {
+    const user = this.findOne(id);
+    this.users = this.users.filter((user) => user.id !== id);
+
+    return user;
+  }
+}

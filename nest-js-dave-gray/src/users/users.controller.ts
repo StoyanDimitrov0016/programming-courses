fc49ea68b83bcddddc6a5c262213cca36a8d3825
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-04-13 20:25:39 +0300

    feat: add users controller CRUD methods

--- nest-js-dave-gray/src/users/users.controller.ts
+++ nest-js-dave-gray/src/users/users.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Param,
+  Query,
+  Get,
+  Post,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  createOne(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id')
+  updateOne(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id')
+  deleteOne(@Param('id') id: string) {
+    return { id };
+  }
+}

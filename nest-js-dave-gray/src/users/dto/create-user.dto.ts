a56e436a5ebf4751692723a698e6d6a12ad695bf
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-05-06 14:03:15 +0300

    feat: add proper validation to create user DTO

--- nest-js-dave-gray/src/users/dto/create-user.dto.ts
+++ nest-js-dave-gray/src/users/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { IsString, IsEmail, IsEnum, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsEnum(['INTERN', 'ENGINEER', 'ADMIN'], {
+    message:
+      'Valid role required. Supported roles: INTERN, ENGINEER, and ADMIN',
+  })
   role: 'INTERN' | 'ENGINEER' | 'ADMIN';
 }
